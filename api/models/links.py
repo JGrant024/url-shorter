a2de702eacdfb63a3dcf34d41b2ebd43e3a72d8e
JGrant024 <jonathan.grant23@icloud.com>
2024-03-05 10:45:31 -0500

    refactored models into smaller chunks

---
+++ api/models/links.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String 
+
+from pydantic import BaseModel 
+
+
+
+class Links(Base): 
+    __tablename__ = "links" 
+    id = Column(Integer, primary_key=True) 
+    title = Column(String) 
+    long_url = Column(String) 
+    short_url = Column(String) 
+    user_id = Column(String ) 
+
+class LinksSchema(BaseModel): 
+    title: str
+    long_url: str 
+    short_url: str  
+    user_id: int 
+
+class Config: 
+    populate_by_name = True 
+        
